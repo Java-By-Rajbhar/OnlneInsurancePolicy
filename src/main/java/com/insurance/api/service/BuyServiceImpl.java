ca41b21630ff8a14f4252a8028bdbe37ee93952d
User1 <User1@LP-5CD9176M8Z.HCLT.CORP.HCL.IN>
2019-08-29 19:04:17 +0530

    laxman

--- src/main/java/com/insurance/api/service/BuyServiceImpl.java
+++ src/main/java/com/insurance/api/service/BuyServiceImpl.java
@@ -18,69 +18,54 @@ public class BuyServiceImpl implements BuyService {
 
 	@Autowired
 	BuyPolicyRepository buyPolicyRepository;
-	
-	
+
 	@Autowired
 	PolicyRepository policyRepository;
-	
+
 	@Autowired
 	PolicyHolderRepository PolicyHolderRepository;
-	
-	@Autowired
-	MailService mailservice;
-	
-	
+
 	@Override
 	public BuyPolicyResponseDto buyPolicy(BuyPolicyRequestDto buyPolicyRequestDto) {
-		BuyPolicyResponseDto buyPolicyResponseDto=new BuyPolicyResponseDto();
-		
-		if(isValidEmailAddress(buyPolicyRequestDto.getEmail()))
-		{
-			PolicyHolder policyHolder=new PolicyHolder();
-			
+		BuyPolicyResponseDto buyPolicyResponseDto = new BuyPolicyResponseDto();
+
+		if (isValidEmailAddress(buyPolicyRequestDto.getEmail())) {
+			PolicyHolder policyHolder = new PolicyHolder();
+
 			policyHolder.setAge(buyPolicyRequestDto.getAge());
 			policyHolder.setEmailId(buyPolicyRequestDto.getEmail());
 			policyHolder.setEmpId(buyPolicyRequestDto.getEmpId());
 			policyHolder.setGender(buyPolicyRequestDto.getGender());
 			policyHolder.setName(buyPolicyRequestDto.getName());
 			policyHolder.setRelationship(buyPolicyRequestDto.getRelationship());
-			PolicyHolder saveData=PolicyHolderRepository.save(policyHolder);
-			
-			
-			
-			
-			
-			BuyPolicy buyPolicy=new BuyPolicy();
+			PolicyHolder saveData = PolicyHolderRepository.save(policyHolder);
+
+			BuyPolicy buyPolicy = new BuyPolicy();
 			buyPolicy.setCustomerId(saveData.getCustomerId());
 			buyPolicy.setPolicyId(buyPolicyRequestDto.getPolicyId());
 			buyPolicy.setPurchaseDate(new Date());
-			
+
 			buyPolicyRepository.save(buyPolicy);
 
-			
 			buyPolicyResponseDto.setMessage("Congrats!!Policy is bought");
 			buyPolicyResponseDto.setStatusCode(200);
 			buyPolicyResponseDto.setStatus("Success");
-			
-		}
-		else
-		{
+
+		} else {
 			buyPolicyResponseDto.setMessage("email id in valid");
 			buyPolicyResponseDto.setStatusCode(404);
 			buyPolicyResponseDto.setStatus("Failed");
 		}
-		
-		
-				
+
 		return buyPolicyResponseDto;
-		
+
 	}
-	
-    public boolean isValidEmailAddress(String email) {
-        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-        java.util.regex.Matcher m = p.matcher(email);
-        return m.matches();
- }
+
+	public boolean isValidEmailAddress(String email) {
+		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+		java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+		java.util.regex.Matcher m = p.matcher(email);
+		return m.matches();
+	}
 
 }
